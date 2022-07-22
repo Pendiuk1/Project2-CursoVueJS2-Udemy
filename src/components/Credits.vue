733ec9f8c73520af0102a21760b5efc88e732f41
Guilherme Pendiuk <pendiuk.guilherme@gmail.com>
2022-07-21 21:10:12 -0300

    project 2 finished

---
+++ src/components/Credits.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="credits-bar">Desenvolvido como parte do curso 
+    <a class="link-curso" href="https://www.udemy.com/course/vue-js-completo/" >Vue JS 2 - O Guia Completo (2022)</a>
+    disponivel na Udemy.
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .credits-bar{
+        position: absolute;
+        bottom: 1px;
+        background-color: #ccc2;
+        width: 100%;
+        text-align: center;
+        color: #aaa;
+    }
+    .link-curso{
+        text-decoration: none;
+        color: #fff;
+    }
+</style>
